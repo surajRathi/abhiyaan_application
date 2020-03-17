6a17519811fadc1198bcb7f7a3369fdaef721f91
Suraj Rathi <suraj.rathi00@gmail.com>
2020-03-17 09:09:43 +0530

    Initial commit. Skeleton arithmetic evaluator.

---
+++ B_1_1_arithmetic-evaluator.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+def evaluate(expr: str) -> float:
+    return float('NaN')
+
+
+def main():
+    expr = input()
+
+    print(evaluate(expr))
+
+
+if __name__ == '__main__':
+    main()

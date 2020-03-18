abd8933b4189519d7c40f8b0b87149449d7ee31b
Suraj Rathi <suraj.rathi00@gmail.com>
2020-03-18 10:00:34 +0530

    evaluvator works

--- B_1_1_arithmetic-evaluator.py
+++ B_1_1_arithmetic-evaluator.py
@@ -5,22 +5,25 @@
     '-': lambda x: -x
 }"""
 
-binary_prec = {
-    '+': 3,
-    '-': 3,
-    '*': 2,
-    '/': 2,
-    '^': 1
+precedence = {
+    '+': 2,
+    '-': 2,
+    '*': 5,
+    '/': 5,
+    '^': 10
 }
 
-binary_operators = {
+binary_operators = {  # IMP: Stack pops second operand before first!!!!
     '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
+    '-': lambda x, y: y - x,
     '*': lambda x, y: x * y,
-    '/': lambda x, y: x / y,
+    '/': lambda x, y: y / x,
+    '^': lambda x, y: y ** x
 }
 
 openers = '([{'
+for opener in openers:
+    precedence[opener] = 0
 closers = ')]}'
 symbols = ''.join(binary_operators.keys()) + openers + closers
 nums = '123456789.'
@@ -40,22 +43,38 @@ def tokenizer(expr: str, num_chars=nums):  # expr: Iterable[char] -> Generator[U
     return  # Required?
 
 
+# Using https://stackoverflow.com/questions/13421424/how-to-evaluate-an-infix-expression-in-just-one-scan-using-stacks
 def evaluate(expr: str) -> float:
     expr = ''.join(filter(lambda ch: ch in (symbols + nums), expr))
     if not expr:
         return float('NaN')  # raise error?
 
-    # noinspection PyTypeChecker
-    tks = tokenizer(expr)
+    operators = []
+    operands = []
+    for t in tokenizer(expr):
+        if isinstance(t, float):
+            operands.append(t)
+        elif t in openers:
+            operators.append(t)
+        elif t in binary_operators:
+            while operators and precedence[operators[-1]] >= precedence[t]:
+                operands.append(binary_operators[operators.pop()](operands.pop(), operands.pop()))
+            operators.append(t)
+        else:
+            opener = openers[closers.index(t)]
+            while (op := operators.pop()) != opener:
+                operands.append(binary_operators[op](operands.pop(), operands.pop()))
 
-    print(list(tks))
-    return float('NaN')
+    while operators:
+        operands.append(binary_operators[operators.pop()](operands.pop(), operands.pop()))
+
+    assert (len(operands) == 1)
+
+    return operands.pop()
 
 
 def main():
-    expr = input()
-
-    print(evaluate(expr))
+    print(evaluate(input()))
 
 
 if __name__ == '__main__':
